ec822a0e518d28d678a89c28103acf6c4606add2
MSI\Admin <m0a23130e5@edu.teu.ac.jp>
2026-01-31 17:22:53 +0900

    マスク追加成功‼‼‼‼‼‼‼‼‼‼

---
+++ Assets/PrivateFolder/Kengo/Script/ObjClickJudge.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System;
+
+public class ObjClickJudge : MonoBehaviour
+{
+    private Renderer rend;
+
+    // 外部から参照できるフラグ
+    public bool IsClicked { get; private set; } = false;
+
+    // 一度だけクリック可能にするフラグ
+    public bool clicked = false;
+
+    // クリック時に通知するイベント
+    public event Action OnClicked;
+
+    private ClickToShowMask showMask;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        // 修正後
+        IsClicked = UnityEngine.Random.value > 0.5f;
+        rend.material.color = IsClicked ? Color.red : Color.blue;
+
+        Debug.Log($"{name} Awake: 初期色 = {(IsClicked ? "赤" : "青")}");
+
+        showMask = GetComponent<ClickToShowMask>();
+    }
+
+    void OnMouseDown()
+    {
+        if (clicked)
+        {
+            Debug.Log($"{name} OnMouseDown: すでにクリック済み");
+            return;
+        }
+
+
+
+        IsClicked = !IsClicked;
+        rend.material.color = IsClicked ? Color.red : Color.blue;
+        clicked = true;
+
+        Debug.Log($"{name} OnMouseDown: クリックされました。IsClicked = {IsClicked}, clicked = {clicked}");
+
+        // クリックされたことを通知
+        showMask.ShowChild();
+    }
+}
