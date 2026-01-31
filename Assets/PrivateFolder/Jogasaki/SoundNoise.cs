9add449b2b5b6c5b6c1e264577ae38c1c3dd3687
m0a23146 <m0a23146ef@edu.teu.ac.jp>
2026-02-01 04:30:11 +0900

    プル用

--- Assets/PrivateFolder/Jogasaki/SoundNoise.cs
+++ Assets/PrivateFolder/Jogasaki/SoundNoise.cs
@@ -5,36 +5,38 @@ public class SoundNoise : MonoBehaviour
 {
     public int outObjNum;
 
-    [SerializeField] private float NoiseVol = -80.0f;
+    [SerializeField] private float NoiseVol;
+    [SerializeField,Header("èâä˙íl")] private float initialValue;
+    [SerializeField, Header("ëùâ¡ó¶")] private float minusNum;
     [SerializeField] private AudioMixer audioMixer;
 
     [SerializeField] private BroadcastAreaManager BroadcastAreaManager;
 
     public AudioSource audioSource;
-    private bool isNoise;
 
     void Start()
     {
+        NoiseVol = initialValue;
         audioSource.Play();
-        //if(BroadcastAreaManager == null)
-        //{
-        //    GameObject gameObject = GameObject.Find("");
-        //    BroadcastAreaManager = gameObject.GetComponent<BroadcastAreaManager>();
-        //}
+        if (BroadcastAreaManager == null)
+        {
+            GameObject gameObject = GameObject.Find("Canvas");
+            BroadcastAreaManager = gameObject.GetComponent<BroadcastAreaManager>();
+        }
     }
 
     void Update()
     {
         //if (Input.GetKey(KeyCode.K)) isNoise = true;
         //else isNoise = false;
-        //NoiseVolume(BroadcastAreaManager.minusCount);
+        NoiseVolume(BroadcastAreaManager.minusCount);
         audioMixer.SetFloat("NoiseVolume", NoiseVol);
     }
 
     public void NoiseVolume(int num)
     {
-        if (NoiseVol < 0.0f) NoiseVol = -80f + 25.0f * num;
-        //SoundManager.Instance.AudioVol = 1.0f - NoiseVol;
+        if (initialValue + minusNum * num < 0.0f) NoiseVol = initialValue + minusNum * num;
+        else if (initialValue + minusNum * num >= 0.0f) NoiseVol = 0f;
     }
 
 }
