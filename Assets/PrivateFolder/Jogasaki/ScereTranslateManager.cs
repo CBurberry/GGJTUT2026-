744d67adbdb72193fa7a5a8120d5b42b7b145df0
wataru tukada <g024g1209@g.neec.ac.jp>
2026-02-01 05:59:46 +0900

    Sliderでの数値によっての色変化＋80fで体感伸びにくく感じる仕組み+30fで減りにくく感じる仕組み

--- Assets/PrivateFolder/Jogasaki/ScereTranslateManager.cs
+++ Assets/PrivateFolder/Jogasaki/ScereTranslateManager.cs
@@ -1,22 +1,41 @@
+using Unity.Burst;
 using UnityEngine;
+
 
 public class ScereTranslateManager : MonoBehaviour
 {
     [SerializeField] private BroadcastAreaManager broadcastAreaManager;
     [SerializeField] private SlliderArea slliderArea;
     [SerializeField] private GameManager gameManager;
-
+    [SerializeField] private float easingSpeed = 5f;
     public float score;
-
+    private float velocity;          // SmoothDampóp
 
     void Start()
     {
-        
     }
-
+    
+ 
     void Update()
     {
-        score = 50 + broadcastAreaManager.score / 200;
+        float targetScore = 50f + broadcastAreaManager.score / 100f;
+
+        float smoothTime = 0.25f;
+        float next = Mathf.SmoothDamp(score, targetScore, ref velocity, smoothTime);
+
+        // 70à»è„ÇÕè„è∏ó Çêßå¿
+        if (score >= 70f)
+        {
+            // 1ïbÇ…10ÇµÇ©è„Ç™ÇÁÇ»Ç¢
+            float maxDelta = 5f * Time.deltaTime; 
+            next = Mathf.Min(next, score + maxDelta);
+        }
+        if (score <= 30f && next < score)
+        {
+            float maxDownDelta = 5f * Time.deltaTime; // 1ïbÇ…5ÇµÇ©â∫Ç™ÇÁÇ»Ç¢
+            next = Mathf.Max(next, score - maxDownDelta);
+        }
+        score = next;
         slliderArea.Score_Slider_controller(score);
     }
 
--- Assets/PrivateFolder/Wataru/SlliderArea.cs
+++ Assets/PrivateFolder/Wataru/SlliderArea.cs
@@ -6,6 +6,18 @@ public class SlliderArea : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private float addValue = 10.0f;
+    [SerializeField] private Image fillImage;
+    private void Start()
+    {
+        if (fillImage == null)
+        {
+            fillImage = slider.fillRect.GetComponent<Image>();
+        }
+    }
+    private void Update()
+    {
+        ChangeSliderColor();
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -24,6 +36,27 @@ public class SlliderArea : MonoBehaviour
             slider.value = Mathf.Clamp(slider.value, slider.minValue, slider.maxValue);
         }
     }
+    private void ChangeSliderColor()
+    {
+
+        if (slider.value <= 15.0f)
+        {
+            fillImage.color = Color.red;
+        }
+        else if (slider.value <= 40.0f)
+        {
+            fillImage.color = Color.yellow;
+        }
+        else if(slider.value <=50.0f)
+        {
+            fillImage.color = Color.green;
+        }
+        else if (slider.value >= 70.0f)
+        {
+            fillImage.color = Color.blue;
+        }
+    }
+
     public void Score_Slider_controller(float score)
     {
         slider.value = score;
