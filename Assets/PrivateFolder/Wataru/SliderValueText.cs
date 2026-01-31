0e8796aa8b07ac9190c123b3dbd384a6e48f82c4
wataru tukada <g024g1209@g.neec.ac.jp>
2026-01-31 17:22:41 +0900

    視聴率バーの仕組み追加（まだ未完成部分あり）

---
+++ Assets/PrivateFolder/Wataru/SliderValueText.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderValueText:MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TMP_Text valueText;
+
+    void Update()
+    {
+        valueText.text = slider.value.ToString("0"); 
+    }
+}
