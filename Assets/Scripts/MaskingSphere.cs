81c2165841ba0881ae0c80a00824466d9538b053
Shunji-Y <chopiliszt@gmail.com>
2026-02-01 13:36:13 +0900

    Adjsting mosaic

--- Assets/Scripts/MaskingSphere.cs
+++ Assets/Scripts/MaskingSphere.cs
@@ -2,4 +2,18 @@ using UnityEngine;
 
 public class MaskingSphere : MonoBehaviour
 {
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 cameradir = Camera.main.transform.position-transform.position;
+        transform.position = startPosition + cameradir*2;
+
+
+    }
 }
