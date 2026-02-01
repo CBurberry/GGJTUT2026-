2a153a8a5bf4aac7d56b4b55b8c4cc2710fe3cee
TKNK <m0a231950e@edu.teu.ac.jp>
2026-02-01 09:43:19 +0900

    視聴率とゲージが連携して値を参照するように

---
+++ Assets/PrivateFolder/Takeki/InputScoreText.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class InputScoreText : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.timeScale != 0)
+        {
+            scoreText.text = slider.value.ToString("F1");
+        }
+    }
+}
