3919b377701191d599a0fe75be96df7e0dd02767
Shunji-Y <chopiliszt@gmail.com>
2026-02-01 13:40:17 +0900

    Masking sphere

--- Assets/Scripts/MaskingSphere.cs
+++ Assets/Scripts/MaskingSphere.cs
@@ -2,18 +2,5 @@ using UnityEngine;
 
 public class MaskingSphere : MonoBehaviour
 {
-    Vector3 startPosition;
 
-    void Start()
-    {
-        startPosition = transform.position;
-    }
-
-    void Update()
-    {
-        Vector3 cameradir = Camera.main.transform.position-transform.position;
-        transform.position = startPosition + cameradir*2;
-
-
-    }
 }
