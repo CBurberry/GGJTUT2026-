5d846a84fb1ef66c1c2676855ff3dda9523c9d71
BurberryC <christianburberry@gmail.com>
2026-02-01 13:33:58 +0900

    Updated the visual viewership representation.

--- Assets/PrivateFolder/Wataru/SlliderArea.cs
+++ Assets/PrivateFolder/Wataru/SlliderArea.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class SlliderArea : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private float addValue = 10.0f;
-    [SerializeField] private Image fillImage;
+
+    public Gradient ColorGradient;
+
+    private Image fillImage;
+
     private void Start()
     {
         if (fillImage == null)
         {
             fillImage = slider.fillRect.GetComponent<Image>();
+            ChangeSliderColor();
         }
-    }
-    private void Update()
-    {
-        ChangeSliderColor();
     }
 
     private void OnTriggerStay(Collider other)
@@ -36,25 +36,10 @@ public class SlliderArea : MonoBehaviour
             slider.value = Mathf.Clamp(slider.value, slider.minValue, slider.maxValue);
         }
     }
-    private void ChangeSliderColor()
-    {
 
-        if (slider.value <= 15.0f)
-        {
-            fillImage.color = Color.red;
-        }
-        else if (slider.value <= 40.0f)
-        {
-            fillImage.color = Color.yellow;
-        }
-        else if(slider.value <=50.0f)
-        {
-            fillImage.color = Color.green;
-        }
-        else if (slider.value >= 70.0f)
-        {
-            fillImage.color = Color.blue;
-        }
+    public void ChangeSliderColor()
+    {
+        fillImage.color = ColorGradient.Evaluate(slider.value / 100f);
     }
 
     public void Score_Slider_controller(float score)
